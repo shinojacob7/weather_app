699135203a9bfd2c333f1a79736006bf5d5046a5
Shino Jacob C X <shinojacob7@gmail.com>
2026-02-28 14:10:28 +0530

    Create weather_service.py with WeatherService class

---
+++ weather_service.py
@@ -0,0 +1,10 @@
+# Weather Service
+
+class WeatherService:
+    def get_weather(self, location):
+        # Logic to get weather data for the location
+        pass
+
+    def get_forecast(self, location):
+        # Logic to get weather forecast for the location
+        pass
