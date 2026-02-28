4a7ebae3d879ad8acf7b12a1fe54313765dadc50
Shino Jacob C X <shinojacob7@gmail.com>
2026-02-28 14:14:32 +0530

    Add WeatherService class with OpenWeatherMap and WeatherAPI integration

--- weather_service.py
+++ weather_service.py
@@ -1,10 +1,21 @@
-# Weather Service
+import requests
 
 class WeatherService:
-    def get_weather(self, location):
-        # Logic to get weather data for the location
-        pass
+    def __init__(self, openweathermap_api_key, weatherapi_key):
+        self.openweathermap_api_key = openweathermap_api_key
+        self.weatherapi_key = weatherapi_key
 
-    def get_forecast(self, location):
-        # Logic to get weather forecast for the location
-        pass
+    def get_openweathermap_data(self, city):
+        url = f'http://api.openweathermap.org/data/2.5/weather?q={{city}}&appid={{self.openweathermap_api_key}}'
+        response = requests.get(url)
+        return response.json()
+
+    def get_weatherapi_data(self, city):
+        url = f'https://api.weatherapi.com/v1/current.json?key={{self.weatherapi_key}}&q={{city}}'
+        response = requests.get(url)
+        return response.json()
+
+    def get_weather(self, city):
+        openweathermap_data = self.get_openweathermap_data(city)
+        weatherapi_data = self.get_weatherapi_data(city)
+        return {'openweathermap': openweathermap_data, 'weatherapi': weatherapi_data}
